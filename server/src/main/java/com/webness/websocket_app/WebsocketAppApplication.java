5d0488d82740286aab6566b8bcaa6525687e5e8a
Marcell Szikora <szikora.marcell@webness.hu>
2025-03-24 20:39:24 +0100

    basic backend code

---
+++ server/src/main/java/com/webness/websocket_app/WebsocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.webness.websocket_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/webness/websocket_app/WebsocketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webness.websocket_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
